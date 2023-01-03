90768322b874612bea1915511fdca2a6fd3ea1c9
azmansabbir <unidevgo.qa1@gmail.com>
2023-01-03 15:00:56 +0600

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -48,7 +48,7 @@ const config: PlaywrightTestConfig = {
   //   open: "never"
   // }], ["allure-playwright"], ['./My-Reporter.js']],
   
-  // globalTeardown: require.resolve("./mailer.js"),
+  globalTeardown: require.resolve("./mailer.js"),
 
 
 
