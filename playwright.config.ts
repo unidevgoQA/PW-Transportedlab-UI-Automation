1bfa524886e5933d20d5f9ca50a0b8d25c7e99eb
azmansabbir <unidevgo.qa1@gmail.com>
2023-02-20 01:48:32 +0600

    Updated Text Cases

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,13 +13,13 @@ const config: PlaywrightTestConfig = {
     "004Menu.test.ts",
     "005SignUp.test.ts",
     "006GlobalPrizing.test.ts",
-    // "007PrizeDrop.test.ts",
+    "007PrizeDrop.test.ts",
     // "008TugOfWar.test.ts",
-    // "009LiveWall.test.ts",    
-    // "010Arcade.test.ts",
-    // "011AddNewExperiences.test.ts",     
-    // "012Trivia.test.ts",
-    // "guesstheScore.test.ts",
+    "009LiveWall.test.ts",    
+    "010Arcade.test.ts",
+    "011AddNewExperiences.test.ts",     
+    "012Trivia.test.ts",
+    "guesstheScore.test.ts",
     // "textExtractFromImage.test.ts"
         
 
@@ -49,7 +49,7 @@ const config: PlaywrightTestConfig = {
     open: "never"
   }], ["allure-playwright"], ['./My-Reporter.js']],
   
-  // globalTeardown: require.resolve("./mailer.js"),
+  globalTeardown: require.resolve("./mailer.js"),
 
 
 
