271fd22257d7eef442a975f4a14d2fe6029ae0ca
azmansabbir <unidevgo.qa1@gmail.com>
2023-03-27 16:57:10 +0600

    Completed FanSee Wall

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,7 +19,7 @@ const config: PlaywrightTestConfig = {
     // "010Arcade.test.ts",
     // "011AddNewExperiences.test.ts",
     "012Trivia.test.ts",
-    // "013Fanatics-filterWeb.test.ts",
+    "013Fanatics-filterWeb.test.ts",
     "014FanSeeWall.test.ts",
     // "guesstheScore.test.ts",
     // // "textExtractFromImage.test.ts"
